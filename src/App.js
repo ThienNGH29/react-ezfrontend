7466d951f433b1694f16c26688b955e1fb85b131
Thien Nguyen Huu <thien@undigital.com.au>
2021-07-04 21:35:26 +0700

    add category + product api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import { NavLink, Redirect, Route, Switch } from "react-router-dom";
-import NotFound from "./components/NotFound";
-import SongFeature from "./features/Song";
-import TodoFeature from "./features/Todo";
+import { useEffect } from 'react';
+import { NavLink, Redirect, Route, Switch } from 'react-router-dom';
+import productApi from './api/productApi';
+import NotFound from './components/NotFound';
+import SongFeature from './features/Song';
+import TodoFeature from './features/Todo';
 
 function App() {
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const params = {
+        _limit: 10,
+      };
+      const productList = await productApi.getAll(params);
+      console.log(productList);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div className="App">
       <h2>Header</h2>
-      <p><NavLink to="/todos">Todos</NavLink></p>
-      <p><NavLink to="/songs">Song</NavLink></p>
-      
+      <p>
+        <NavLink to="/todos">Todos</NavLink>
+      </p>
+      <p>
+        <NavLink to="/songs">Song</NavLink>
+      </p>
+
       <Switch>
-        <Redirect from="/home" to="/" exact/>
-        <Redirect from="/post-list/:postId" to ="/posts/:postId" exact />
+        <Redirect from="/home" to="/" exact />
+        <Redirect from="/post-list/:postId" to="/posts/:postId" exact />
 
         <Route path="/" component={SongFeature} exact />
         <Route path="/todos" component={TodoFeature} />

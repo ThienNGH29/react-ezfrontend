11e5bd8e5943b396d1906d8dabe4c2480bbd2fcd
Thien Nguyen Huu <thien@undigital.com.au>
2021-07-06 22:46:37 +0700

    add redux/redux-toolkit + add jsconfig

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { NavLink, Redirect, Route, Switch } from 'react-router-dom';
 import productApi from './api/productApi';
 import NotFound from './components/NotFound';
+import CounterFeature from './features/Counter';
 import SongFeature from './features/Song';
 import TodoFeature from './features/Todo';
 
@@ -20,7 +21,6 @@ function App() {
 
   return (
     <div className="App">
-      <h2>Header</h2>
       <p>
         <NavLink to="/todos">Todos</NavLink>
       </p>
@@ -32,13 +32,12 @@ function App() {
         <Redirect from="/home" to="/" exact />
         <Redirect from="/post-list/:postId" to="/posts/:postId" exact />
 
-        <Route path="/" component={TodoFeature} exact />
+        <Route path="/" component={CounterFeature} exact />
         <Route path="/todos" component={TodoFeature} />
         <Route path="/songs" component={SongFeature} />
 
         <Route component={NotFound} />
       </Switch>
-      <h2>Footer</h2>
     </div>
   );
 }
---
+++ src/features/Counter/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrease, increase } from './counterSlice';
+
+function CounterFeature(props) {
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch()
+
+  const handleIncrease = () => {
+    const action = increase(); // action creator
+    dispatch(action)
+  }
+
+  const handleDecrease = () => {
+    const action = decrease(); // action creator
+    dispatch(action)
+  }
+
+  return (
+    <div>
+      <h2>Counter Feature</h2>
+      <p>Counter: {counter}</p>
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleDecrease}>Decrease</button>
+    </div>
+  );
+}
+
+export default CounterFeature;

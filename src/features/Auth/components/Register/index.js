8d481a872b86e3ac468490e0b7ddfcbd51c7a452
Thien Nguyen Huu <thien@undigital.com.au>
2021-07-18 17:01:53 +0700

    add register feature

--- src/features/Auth/components/Register/index.js
+++ src/features/Auth/components/Register/index.js
@@ -1,19 +1,33 @@
+import { unwrapResult } from '@reduxjs/toolkit';
+import { register } from 'features/Auth/userSlice';
 import React from 'react';
-import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import RegisterForm from '../RegisterForm';
 
-Register.propTypes = {
-  
-};
+Register.propTypes = {};
 
 function Register(props) {
-  const handleSubmit = (values) => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = async (values) => {
     console.log('Register submit: ', values);
-  }
+    try {
+      // auto set usernam = email
+      values.username = values.email;
+
+      const action = register(values);
+      const resultAction = await dispatch(action);
+      const user = unwrapResult(resultAction);
+      
+      console.log('New user', user);
+    } catch (error) {
+      console.log('Failed to register: ', error);
+    }
+  };
 
   return (
     <div>
-      <RegisterForm onSubmit={handleSubmit}/>
+      <RegisterForm onSubmit={handleSubmit} />
     </div>
   );
 }
